f9c6c894b636426e6a803c29f86c8d6bc96104a0
chuma6619 <chuma6619@gmail.com>
2022-04-24 22:08:59 +0100

    column correction

--- src/Components/Userprofile.jsx
+++ src/Components/Userprofile.jsx
@@ -10,11 +10,11 @@ const Userprofile = ({userlist}) => {
                 userlist.map((userprofile) => (
                   <div className="col-md-6 col-sm-12" key={userprofile.id}>
                     <div className=' px-5 pt-3 pb-2 mb-5 profile-container'>
-                      <h1 className="name-header fw-bolder ps-lg-5 ps-md-5 ps-sm-0"> {userprofile.name}</h1>
-                      <p className="user-text pt-2 mb-5 ps-lg-5 ps-md-5 ps-sm-0">I'm a disigner in love with photography, 
+                      <h1 className="name-header fw-bolder ps-lg-5 ps-md-0 ps-sm-0"> {userprofile.name}</h1>
+                      <p className="user-text pt-2 mb-5 ps-lg-5 ps-md-0 ps-sm-0">I'm a disigner in love with photography, 
                       painting and discovering new worlds and cultures.
                       </p>
-                      <div className="info-container ps-lg-5 ps-md-5 ps-sm-0">
+                      <div className="info-container ps-lg-5 ps-md-0 ps-sm-0">
                         <h3 className='pb-3'>General Info</h3>
                         <div className="row">
 
